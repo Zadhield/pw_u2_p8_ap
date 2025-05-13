2d887a34be808c29a5a2d5dec2d54a2be6038a40
Zadhield <afpujota@uce.edu.ec>
2025-05-12 20:25:55 -0500

    Taller12

---
+++ src/components/HolaMundo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <h1 class="titulo">Universidad Central del Ecuador</h1>
+    <h2>Facultad de Ingeniería</h2>
+    <h3>Carrera de Computaci&#243;n</h3>
+    <h4>Hola mundo desde Vue</h4>
+    <p>Angelo Pujota</p>
+  </div>
+</template>
+ 
+<script>
+export default {
+ 
+}
+</script>
+ 
+<style>
+.titulo {
+    color: red;
+}
+ 
+h2 {
+    background: blue;
+}
+ 
+p {
+    color: green;
+    background: blue;
+    font-family: 'Times New Roman', Times, serif;
+    font-size: 40px;
+}
+</style>
---
+++ src/views/HolaMundoView.vue
@@ -0,0 +1,16 @@
+<template>
+  <hm/>
+</template>
+
+<script>
+import hm from '../components/HolaMundo.vue'
+export default {
+    components:{
+        hm
+    }
+}
+</script>
+
+<style>
+
+</style>
