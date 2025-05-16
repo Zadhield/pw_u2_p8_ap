c061fc3974bef4f266e574e5e0b2eeea7c1307c4
Zadhield <afpujota@uce.edu.ec>
2025-05-15 23:46:38 -0500

    Tarea 3

--- src/components/Estudiante.vue
+++ src/components/Estudiante.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="container">
     <label for="id_nombre"> Nombre:</label>
-     <label for="id_apellido"> Nombre:</label>
     <input v-model="nuevoNombre" id="id_nombre" type="text" />
-     <input v-model="nuevoApellido" id="id_apellido" type="text" />
-    <button v-on:click ="agregarEstudiante()">Agregar Estudiante</button>
-    {{ nuevoNombre }}
+
+    <label for="id_apellido"> Apellido:</label>
+    <input v-model="nuevoApellido" id="id_apellido" type="text" />
+
+    <label for="id_edad">Edad:</label>
+    <input v-model="nuevaEdad" id="id_edad" type="number" />
+
+    <label for="id_carrera">Carrera:</label>
+    <input v-model="nuevaCarrera" id="id_carrera" type="text" />
+
+    <label for="id_correo">Correo:</label>
+    <input v-model="nuevoCorreo" id="id_correo" type="email" />
+
+    <button v-on:click="agregarEstudiante()">Agregar Estudiante</button>
+
+
     <ul>
-      <li v-for="{ nombre, apellido } in lista" :key="nombre">
-        Nombre: {{ nombre }} -Apellido:{{ apellido }}
+      <li v-for="{nombre, apellido, edad, carrera, correo} in lista" :key="nombre">
+       <strong>Nombre:</strong>{{ nombre }}- <strong>Apellido:</strong>{{ apellido }}- <strong> Edad:</strong>{{ edad }}-
+        <strong>Carrera:</strong> {{ carrera }} <strong>-Correo:</strong>{{ correo }}
       </li>
     </ul>
   </div>
@@ -18,30 +31,95 @@
 export default {
   data() {
     return {
-      nuevoNombre: "Nombre",
-      nuevoApellido:"Apellido",
+      nuevoNombre: "",
+      nuevoApellido: "",
+      nuevaEdad: "",
+      nuevaCarrera: "",
+      nuevoCorreo: "",
       lista: [
-        { nombre: "Angelo", apellido: "Pujota" },
-        { nombre: "Pedro", apellido: "Guaycha" },
-        { nombre: "Juan", apellido: "Teran" },
-        { nombre: "Marco", apellido: "Andrade" },
-        { nombre: "Andrea", apellido: "Castillo" },
-        { nombre: "Marcelo", apellido: "Lopez" },
-        { nombre: "Alejandra", apellido: "Pineda" },
-      ],
+  {
+    nombre: "Angelo",
+    apellido: "Pujota",
+    edad: 22,
+    carrera: "Ingeniería",
+    correo: "angelo@example.com",
+  },
+  {
+    nombre: "Pedro",
+    apellido: "Guaycha",
+    edad: 23,
+    carrera: "Administración",
+    correo: "pedro.guaycha@example.com",
+  },
+  {
+    nombre: "Juan",
+    apellido: "Teran",
+    edad: 21,
+    carrera: "Psicología",
+    correo: "juan.teran@example.com",
+  },
+  {
+    nombre: "Marco",
+    apellido: "Andrade",
+    edad: 24,
+    carrera: "Medicina",
+    correo: "marco.andrade@example.com",
+  },
+  {
+    nombre: "Andrea",
+    apellido: "Castillo",
+    edad: 20,
+    carrera: "Derecho",
+    correo: "andrea.castillo@example.com",
+  },
+  {
+    nombre: "Marcelo",
+    apellido: "Lopez",
+    edad: 25,
+    carrera: "Arquitectura",
+    correo: "marcelo.lopez@example.com",
+  },
+  {
+    nombre: "Alejandra",
+    apellido: "Pineda",
+    edad: 22,
+    carrera: "Diseño Gráfico",
+    correo: "alejandra.pineda@example.com",
+  },
+],
     };
   },
-  methods:{
-    agregarEstudiante(){
-        const nuevo= {
-            nombre:this.nuevoNombre,
-            apellido:this.nuevoApellido,
-        }
-        this.lista.unshift(nuevo);
-    }
-  }
+  methods: {
+     agregarEstudiante() {
+      const nuevo = {
+        nombre: this.nuevoNombre,
+        apellido: this.nuevoApellido,
+        edad: this.nuevaEdad,
+        carrera: this.nuevaCarrera,
+        correo: this.nuevoCorreo,
+      };
+      this.lista.unshift(nuevo);
+    },
+  },
 };
 </script>
 
 <style>
+.container{
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  max-width: 400px;
+  margin: 0 auto;
+}
+button {
+  padding: 8px;
+  background-color: #3498db;
+  color: white;
+  border: solid;
+}
+ul {
+  margin-top: 20px;
+  text-align: left;
+}
 </style>
